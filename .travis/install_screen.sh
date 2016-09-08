70d8998714b300a75465266f9faf2c55e8d504ea
yan <yan@srtd.org>
2016-09-08 11:57:21 -0400

    Attempts++

--- .travis/install_screen.sh
+++ .travis/install_screen.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    sudo apt-add-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main" -y
-    sudo apt-get update -q 
-    sudo apt-get install --force-yes -y clang-3.8 lldb-3.8 
-    sudo apt-get clean
-    sudo apt-get autoremove 
-fi
-
-
 # First, install bear
 mkdir -p bear/build
 
@@ -18,7 +9,7 @@ wget -O Bear-2.2.0.tar.gz https://github.com/rizsotto/Bear/archive/2.2.0.tar.gz
 tar xfz Bear-2.2.0.tar.gz --strip-components=1 -C bear
 
 pushd bear/build
-cmake .. -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++
+cmake .. 
 make 
 make install
 popd
