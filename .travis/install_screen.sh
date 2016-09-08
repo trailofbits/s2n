54dd0de9443fa650af35bc1474f9dfd5b23c5ced
yan <yan@srtd.org>
2016-09-08 12:12:43 -0400

    Add some debug output

--- .travis/install_screen.sh
+++ .travis/install_screen.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-set -e
+# set -e
+
+# onerr() {
+#     echo "Errored"
+# }
+# trap onerr ERR
 
 # First, install bear
 mkdir -p bear/build
@@ -9,6 +14,12 @@ wget -O Bear-2.2.0.tar.gz https://github.com/rizsotto/Bear/archive/2.2.0.tar.gz
 tar xfz Bear-2.2.0.tar.gz --strip-components=1 -C bear
 
 pushd bear/build
+echo CC: $CC
+echo CXX: $CXX
+echo PATH: $PATH
+clang -v
+clang-3.8 -v 
+
 cmake .. 
 make 
 make install
