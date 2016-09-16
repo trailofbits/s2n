b1cd300da1badeee0f3bf024945e88ce038f6141
Sophia D'Antoine <smdantoine@gmail.com>
2016-09-16 12:38:51 -0400

    added cmake files

--- .travis/invoke_screen.sh
+++ .travis/invoke_screen.sh
@@ -22,28 +22,8 @@ time ./screen/build/llvm/bin/opt \
   -screen-start-symbol ${START_SYM} ${LIB} -o ${LIB_PATH}/xformed.bc || true
 
 # TODO run pagai on bc, assuming on linux and this build works on travis's 14.04 
-#wget https://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz/download
-#tar zxf download
-#cd  boost_1_58_0
-#./bootstrap.sh --exec-prefix=/usr/local --libdir=/usr/lib/x86_64-linux-gnu --includedir=/usr/lib/x86_64-linux-gnu --with-libraries=atomic,date_time,exception,filesystem,iostreams,locale,program_options,regex,signals,system,test,thread,timer,log
-#./b2 link=static
-#sudo ./b2 install
-#cd ..
-# Install Z3
-#wget "https://github.com/Z3Prover/z3/archive/29606b5179f76783ffb0c2ca0ed9d614847064b3.tar.gz" -O z3-29606b5179f76783ffb0c2ca0ed9d614847064b3.tar.gz
-#tar zxf z3-29606b5179f76783ffb0c2ca0ed9d614847064b3.tar.gz 
-#cd z3-29606b5179f76783ffb0c2ca0ed9d614847064b3
-#CXX=clang++ CC=clang python scripts/mk_make.py --prefix=/usr/local
-#cd build
-#make
-#sudo make install
-#cd ../../
 echo "cat screen_output.txt"
 cat screen_output.txt
-#sudo apt-get install chrpath
-#sudo chrpath -r "./screen/pagai/pagai_dynamic_libs/" ./screen/pagai/src/pagai
-#export LD_LIBRARY_PATH=./screen/pagai/pagai_dynamic_libs/
-#echo $LD_LIBRARY_PATH
 pushd ./screen/pagai2
 cmake .
 make
