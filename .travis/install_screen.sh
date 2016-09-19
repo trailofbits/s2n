fb244465edd20054cdbfc254c64be167e8867d86
Sophia D'Antoine <smdantoine@gmail.com>
2016-09-19 18:18:57 -0400

    installing boost

--- .travis/install_screen.sh
+++ .travis/install_screen.sh
@@ -14,6 +14,15 @@ pushd screen
 ./build.sh
 popd
 
-echo "installing dependancies for pagai"
+echo "Installing dependancies for pagai"
 sudo apt-get install libapron libapron-dev libapron-ocaml-dev 
 # need cudd yices and boost, export path in invoke script
+
+echo "Installing boost"
+wget http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz?r=&ts=1474322412&use_mirror=heanet
+mv boost_1_58_0.tar.gz\?r\= boost_1_58_0.tar.gz
+tar zxf boost_1_58_0.tar.gz
+pushd boost_1_58_0/
+./bootstrap.sh
+./b2 install
+popd
