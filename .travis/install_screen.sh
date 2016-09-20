70c3a8546e4e570c03276d4381891fc835e46142
Sophia D'Antoine <smdantoine@gmail.com>
2016-09-20 12:28:37 -0400

    fetch from s3 bucket

--- .travis/install_screen.sh
+++ .travis/install_screen.sh
@@ -9,12 +9,12 @@ trap onerr ERR
 
 echo "Installing screen"
 rm -rf screen
-git clone https://${GH_TOKEN}@github.com/trailofbits/screen.git
+git clone https://${GH_TOKEN}@github.com/trailofbits/screen.git -b prebuild-deps 
 pushd screen
 ./build.sh
 popd
 
 echo "Installing dependancies for pagai"
-sudo apt-get install libapron libapron-dev libapron-ocaml-dev libmpfr-dev flex bison libncurses-dev libgmp3-dev zlib1g-dev
+#sudo apt-get install libapron libapron-dev libapron-ocaml-dev libmpfr-dev flex bison libncurses-dev libgmp3-dev zlib1g-dev
 # need cudd yices and boost, export path in invoke script
 
